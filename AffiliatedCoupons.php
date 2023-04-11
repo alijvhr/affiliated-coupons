447ef5b0bb102278f7ad192867b85ba378f2aabd
Alijvhr <eng.alijvhr@gmail.com>
2023-04-11 12:18:54 +0330

    Initial commit

---
+++ AffiliatedCoupons.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: afiiliated-coupons
+ * Plugin URI: https://github.com/alijvhr/afiiliated-coupons
+ * Description: This plugin let you provide a coupon creation dashboard to your affiliate with max discount percent of his affiliation percent.
+ * Version: 0.1
+ * Author: alijvhr
+ * Author URI: https://alijvhr.ir/
+ **/
